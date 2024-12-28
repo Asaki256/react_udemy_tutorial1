717011a49b016feac684227ce1e5a8a8905fef58
Masaki <ryota423.jp@icloud.com>
2024-12-28 23:47:52 +0900

    arrow関数について

--- 03_js_basic/020_arrow_function/start/main.js
+++ 03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,21 @@
 function fn(number) {
   return number * 2;
 }
+
+
+console.log(fn(3));
+console.log(fn(6));
+
+// const fnArrow = (number, number2) => {
+//   return number * number2 * 10;
+// }
+
+const fnArrow = number => {
+  number += 10;
+  return number * 2;
+}
+const fnArrowObj = number => ({ result: number * 2 });
+
+console.log(fnArrowObj(12));
+
+console.log(fnArrow(40));
